4d89d140bda6f56fc61c14f44f7ef58e9800dd4d
santa1264-hash <265767822+santa1264-hash@users.noreply.github.com>
2026-04-28 10:39:51 +1000

    Adjust print vitals layout

--- ui/rem_card_sectors/s_print/vitals.py
+++ ui/rem_card_sectors/s_print/vitals.py
@@ -7,8 +7,20 @@ from .table_layout import (
     table_width_attrs,
 )
 
-VITALS_NAME_WIDTH_PT = 50.0
-VITALS_CELL_STYLE = "font-size: 5pt; line-height: 1; padding-left: 0; padding-right: 0;"
+VITALS_NAME_WIDTH_PT = 120.0
+VITALS_CELL_STYLE = "font-size: 6pt; line-height: 1; padding-left: 0; padding-right: 0;"
+BP_CELL_STYLE = "line-height: 0.95;"
+
+
+def _format_bp_cell(sys, dia):
+    has_sys = sys not in (None, "")
+    has_dia = dia not in (None, "")
+    if not has_sys and not has_dia:
+        return ""
+
+    sys_text = str(sys) if has_sys else "&nbsp;"
+    dia_text = str(dia) if has_dia else "&nbsp;"
+    return f'<span style="{BP_CELL_STYLE}">{sys_text}<br/>{dia_text}</span>'
 
 
 def render_vitals(data, hours, table_width_pt):
@@ -51,13 +63,13 @@ def render_vitals(data, hours, table_width_pt):
             if key == "ad":
                 sys = v_matrix.get(i, {}).get("sys", "")
                 dia = v_matrix.get(i, {}).get("dia", "")
-                if sys or dia: val = f"{sys}/{dia}"
+                val = _format_bp_cell(sys, dia)
             elif key == "cvp":
                 cvp_val = v_matrix.get(i, {}).get("cvp", "")
                 val = "Н/Н" if cvp_val == -1 else cvp_val
             else:
                 val = v_matrix.get(i, {}).get(key, "")
-            extra_style = VITALS_CELL_STYLE + (" white-space: nowrap;" if key == "ad" else "")
+            extra_style = VITALS_CELL_STYLE
             row += f'<td class="matrix-cell" {cell_attrs(matrix_widths[i], extra_style)}>{cell_content(val)}</td>'
         html += row + '</tr>'
     html += '</tbody></table></div>'
