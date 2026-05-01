699fe831e12704f75cdf10dfcf8ef864991d1293
santa1264-hash <265767822+santa1264-hash@users.noreply.github.com>
2026-05-01 17:52:10 +1000

    Remove restored order domain service artifact

--- services/order_domain_service_restored.py
+++
@@ -1,6 +0,0 @@
-# uncompyle6 version 3.9.3
-# Python bytecode version base 3.11 (3495)
-# Decompiled from: Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)]
-# Embedded file name: services\order_domain_service.py
-# Compiled at: 2026-04-19 08:51:35
-# Size of source mod 2**32: 21526 bytes
