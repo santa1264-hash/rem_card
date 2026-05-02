6ddad838b9ede9e1ceec7e30b58a6d147d73f455
santa1264-hash <265767822+santa1264-hash@users.noreply.github.com>
2026-05-02 21:48:14 +1000

    Remove legacy jornal launcher alias

--- launcher.py
+++ launcher.py
@@ -1,23 +1,5 @@
-import sys
-import os
-
 from _local_rem_card_bootstrap import bootstrap_local_rem_card
 
-
-def _strip_legacy_jornal_role(argv):
-    normalized = [argv[0]]
-    i = 1
-    while i < len(argv):
-        arg = argv[i]
-        if arg == "--role" and i + 1 < len(argv) and str(argv[i + 1]).lower() == "jornal":
-            i += 2
-            continue
-        if arg.startswith("--role=") and arg.split("=", 1)[1].lower() == "jornal":
-            i += 1
-            continue
-        normalized.append(arg)
-        i += 1
-    return normalized
 
 def run_rem_card():
     bootstrap_local_rem_card()
@@ -26,5 +8,4 @@ def run_rem_card():
     main()
 
 if __name__ == "__main__":
-    sys.argv = _strip_legacy_jornal_role(sys.argv)
     run_rem_card()
